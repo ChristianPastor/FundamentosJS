d453779dbf2bf88e96da5a198ea038e015557619
ChristianPastor <christianpastorh@gmail.com>
2020-02-14 15:11:13 -0600

    funtions

---
+++ function.js
@@ -0,0 +1,8 @@
+function imprimiredad(n, e){
+    console.log(`${n} tiene ${e}`);
+}
+
+imprimiredad('CArlos de la garza', 23)
+imprimiredad('Alexander', 23)
+imprimiredad('Diego ', 23)
+imprimiredad('', 23)
