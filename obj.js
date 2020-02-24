f9d76aa7842d7aa72c9b254a217a4447b9161d25
ChristianPastor <christianpastorh@gmail.com>
2020-02-24 10:46:16 -0600

    clase(

--- obj.js
+++ obj.js
@@ -23,6 +23,17 @@ function printNameUpperCase(persona){
 function printNameFull(persona){
    console.log(`Hola me llamo ${persona.nombre} y tengo ${persona.edad}.`);
 }
+function remplaceTobirthday (persona){
+    // se sumara uno al volor de la edad actual y se remplazzara
+    persona.edad +=1;
+}
+
+function newObjPerson(persona){
+    return {
+        ...persona,
+        edad: persona.edad +1
+    }
+}
 
 printNameUpperCase(Sacha)
 printNameUpperCase(dario)
