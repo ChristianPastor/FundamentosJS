083a4c5014b5a64a1bd58edd70eb7fab462514f5
ChristianPastor <christianpastorh@gmail.com>
2020-03-10 18:53:23 -0600

    Clases

--- clase21.js
+++ clase21.js
@@ -1,11 +1,18 @@
-function Persona (nombre, apellido){
+function Persona (nombre, apellido, altura){
    this.nombre = nombre
    this.apellido = apellido
+   this.edad = 18
+   this.altura = altura
 }
 
 Persona.prototype.saludar = function (){
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
-var pastor = new Persona('Christian', 'Pastor')
+Persona.prototype.soyALto = function (){
+    this.altura > 1.8 ? console.log(`Soy alto`) : console.log(`No soy alto`)
+}
+var pastor = new Persona('Christian', 'Pastor',1.92)
+var erik = new Persona('Erik', 'Hernandez',1.81)
+var alondra = new Persona('Alondra', 'Alvarez',1.7)
 pastor.saludar()
 
